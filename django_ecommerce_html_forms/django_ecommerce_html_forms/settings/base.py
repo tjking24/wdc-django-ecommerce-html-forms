f0ed4c70a4deaf4bff4875c93fbf062dee11e5aa
Martin Zugnoni <martin.zugnoni@gmail.com>
2018-11-27 10:45:05 -0300

    Add STATIC_ROOT setting

--- django_ecommerce_html_forms/django_ecommerce_html_forms/settings/base.py
+++ django_ecommerce_html_forms/django_ecommerce_html_forms/settings/base.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DJANGO_PROJECT_DIR = os.path.dirname(BASE_DIR)
+PROJECT_DIR = os.path.dirname(DJANGO_PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
